671011be108716c8daec5bf542fae347feb7bca2
F4nnur <Fann1kF@yandex.ru>
2021-07-02 14:33:25 +0300

    Added Bd and configured admin

--- Django/Pratice_summer/site_with_tests/models.py
+++ Django/Pratice_summer/site_with_tests/models.py
@@ -2,9 +2,39 @@ from django.db import models
 
 
 # Create your models here.
+class Question(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, blank=False, null=False)
+    quest = models.TextField(blank=True)
+    # сделать test_id null=true и переделать наследование id Вопросы -> Тесты -> прохождение
+    test = models.ForeignKey('Test', on_delete=models.DO_NOTHING, related_name='testidforquest', null=True)
+    var1 = models.CharField(max_length=255)
+    var2 = models.CharField(max_length=255)
+    var3 = models.CharField(max_length=255)
+    var4 = models.CharField(max_length=255)
+    true_var = models.IntegerField()
 
-class Table_of_tests(models.Model):
-    num_of_test = models.AutoField(pri)
-    content = models.TextField(blank=True)
-    id_of_test = models.AutoField(primary_key=True)
+    class Meta:
+        verbose_name = 'Вопросы'
+        verbose_name_plural = 'Вопросы'
+
+
+class Test(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, blank=False, null=False)
+    user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
+    name_of_test = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'Тесты'
+        verbose_name_plural = 'Тесты'
+
+
+class Entering(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    test = models.ForeignKey('Test', on_delete=models.DO_NOTHING, related_name='testidforentering')
+    quest = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='questionid')
+    answer_of_user = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Прохождение'
+        verbose_name_plural = 'Прохождение'
