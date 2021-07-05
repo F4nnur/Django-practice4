2b49427bac49f65ffa174d8cd794070c94d64177
F4nnur <Fann1kF@yandex.ru>
2021-07-05 13:20:16 +0300

    Adedd all urls

--- Django/Pratice_summer/site_with_tests/urls.py
+++ Django/Pratice_summer/site_with_tests/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', inndex, name='home')
+    path('', inndex, name='home'),
+    path('configure/', configure_test, name='configure'),
+    path('editing/', editing_test, name='editing'),
+    path("all_tests/", all_tests, name='all_tests'),
+    path('passing/', passing_test, name='passing_test'),
+    path('results/', results_test, name='results'),
+    path("statistics/", statistics, name='statistics'),
 ]
--- Django/Pratice_summer/site_with_tests/views.py
+++ Django/Pratice_summer/site_with_tests/views.py
@@ -7,3 +7,27 @@ from django.shortcuts import render
 def inndex(request):
     return render(request, 'site_with_tests/index.html')
 
+
+def configure_test(request):
+    return render(request, 'site_with_tests/configure_test.html')
+
+
+def editing_test(request):
+    return render(request, 'site_with_tests/editing.html')
+
+
+def all_tests(request):
+    return render(request, 'site_with_tests/all_tests.html')
+
+
+def passing_test(request):
+    return render(request, 'site_with_tests/passing_test.html')
+
+
+def results_test(request):
+    return render(request, 'site_with_tests/results_test.html')
+
+
+def statistics(request):
+    return render(request, 'site_with_tests/statistics.html')
+
